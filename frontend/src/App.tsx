1fba3fcac08830923a8a950595868d2dcaf7f2fc
Deyvison Silva <deyvisondasilva@gmail.com>
2022-11-15 13:05:59 -0300

    Create first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Hello, Wolrd!</h1>
+    <>
+      <h1>Hello, Wolrd!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
