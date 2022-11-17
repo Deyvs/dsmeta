5407721d17562794efa801d4e63f94fd4d52e470
Deyvison Silva <deyvisondasilva@gmail.com>
2022-11-16 23:24:27 -0300

    Search sales by date range

--- backend/src/main/java/com/github/Deyvs/dsmeta/repository/SaleRepository.java
+++ backend/src/main/java/com/github/Deyvs/dsmeta/repository/SaleRepository.java
@@ -1,7 +1,15 @@
 package com.github.Deyvs.dsmeta.repository;
 
 import com.github.Deyvs.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :minDate AND :maxDate ORDER BY obj.amount DESC")
+    Page<Sale> findSales(LocalDate minDate, LocalDate maxDate, Pageable pageable);
 }
--- backend/src/main/java/com/github/Deyvs/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/github/Deyvs/dsmeta/service/SaleService.java
@@ -7,14 +7,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Service
 public class SaleService {
 
     @Autowired
     private SaleRepository repository;
 
-    public Page<Sale> findSales(Pageable pageable) {
-        return repository.findAll(pageable);
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.now(ZoneId.systemDefault());
+
+        LocalDate startDate = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+        LocalDate finishDate = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(startDate, finishDate, pageable);
     }
 
 }
