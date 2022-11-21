abc6c1a9db6322ee02b0e6a7cb0df12f78c0b5ff
Deyvison Silva <deyvisondasilva@gmail.com>
2022-11-21 16:01:53 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,20 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./style.css";
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => { console.log("SUCESSO!!!") })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-redbutton-form">
+        <div className="dsmeta-redbutton-form" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
